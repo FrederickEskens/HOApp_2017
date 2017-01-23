b6f8556d5dd444fd87b1532191fb522cfe8a2a1f
Frederick Eskens <feskens@reference.be>
2017-01-23 13:45:46 +0100

    Main screen filler text

--- HOApp_2017/HOApp_2017.Droid/MainActivity.cs
+++ HOApp_2017/HOApp_2017.Droid/MainActivity.cs
@@ -1,15 +1,13 @@
-﻿using System;
+﻿using System.Text;
 
 using Android.App;
-using Android.Content;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
 
 namespace HOApp_2017.Droid
 {
-	[Activity (Label = "HOApp_2017.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "HO 2017", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
 		protected override void OnCreate (Bundle bundle)
@@ -19,6 +17,18 @@ namespace HOApp_2017.Droid
             RequestWindowFeature(WindowFeatures.NoTitle);
 
             SetContentView (Resource.Layout.Main);
-        }
+
+		    var body = FindViewById<TextView>(Resource.Id.body);
+
+            var sb = new StringBuilder();
+		    sb.AppendLine(
+		        "Herfstontmoeting is het startweekend voor alle leiding van Scouts en Gidsen Vlaanderen. Deze app is jouw digitale gids doorheen het weekend.");
+            sb.Append(System.Environment.NewLine);
+            sb.AppendLine("Tip: gebruik de knop links bovenaan om het menu te openen.");
+            sb.Append(System.Environment.NewLine);
+            sb.AppendLine("Veel plezier!");
+
+		    body.Text = sb.ToString();
+		}
 	}
 }
