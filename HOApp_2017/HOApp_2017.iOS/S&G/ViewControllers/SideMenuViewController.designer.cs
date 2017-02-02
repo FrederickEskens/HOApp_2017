1b1589218b335a67398122624bc2b51b00c43aa5
Jeroen Crevits <jeroen@bazookas.be>
2017-02-02 20:32:36 +0100

    SideNavigation added

---
+++ HOApp_2017/HOApp_2017.iOS/S&G/ViewControllers/SideMenuViewController.designer.cs
@@ -0,0 +1,34 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace HOApp_2017.iOS
+{
+	[Register ("SideMenuViewController")]
+	partial class SideMenuViewController
+	{
+		[Outlet]
+		UIKit.UIButton btnIntro { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnProgramm { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnIntro != null) {
+				btnIntro.Dispose ();
+				btnIntro = null;
+			}
+
+			if (btnProgramm != null) {
+				btnProgramm.Dispose ();
+				btnProgramm = null;
+			}
+		}
+	}
+}
--- HOApp_2017/HOApp_2017/ScoutsEnGidsen/HO/BL/Controllers/NavigationController.cs
+++ HOApp_2017/HOApp_2017/ScoutsEnGidsen/HO/BL/Controllers/NavigationController.cs
@@ -1,10 +1,64 @@
 ﻿namespace HOApp_2017.ScoutsEnGidsen.HO.BL.Controllers {
 	public class NavigationController {
+		#region delegates
+		public delegate void GoToPageDelegate(PagesEnum page, BaseContentController controller);
+		#endregion
 
-		public delegate void GoToPageDelegate();
+		#region events
+		public event GoToPageDelegate GoToPageEvent;
+		#endregion
+		#region variables
 
-		enum Pages {
-			MAIN
+		#endregion
+
+		#region constructor
+
+		#endregion
+
+		#region properties
+
+		#endregion
+
+		#region public methods
+		public void SideMenuButtonClicked(PagesEnum page)
+		{
+			BaseContentController controller;
+			switch (page)
+			{
+				case PagesEnum.INTRO:
+					controller = new IntroPageController();
+					break;
+				default:
+					controller = null;
+					break;
+			}
+			if (GoToPageEvent != null)
+				GoToPageEvent(page, controller);
+		}
+		#region override methods
+
+		#region Viewcycle
+
+		#endregion
+
+		#endregion
+
+		#endregion
+
+		#region private methods
+
+		#endregion
+
+
+		public enum PagesEnum {
+			INTRO,
+			PROGRAM,
+			MAP,
+			INFO,
+			LIVERULES,
+			YEARSONG,
+			SETTINGS,
+			ABOUT
 		}
 	}
 }
